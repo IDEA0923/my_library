591d11189a883363158fc6a3e25528417f7ed284
IDEA <jjggsfx@gmail.com>
2025-06-01 16:38:30 +1000

    V01

---
+++ server_net.hpp
@@ -0,0 +1,16 @@
+#include "default.hpp"
+
+//for the work with net
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cstring>
+
+int start(int port){
+    int sock = socket(AF_INET , SOCK_STREAM , 0);
+    sockaddr_in  setting ;
+    setting.sin_family = AF_INET ;
+    setting.sin_port = htons(port);
+    
+}
